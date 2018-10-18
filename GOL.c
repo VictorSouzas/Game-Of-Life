f23221d80a1102c3138a799efb2d7d9c89bd4f30
victor souza <victor_souzas@yahoo.com.br>
2018-10-18 01:20:03 -0300

    attempt to use thread fail #1

--- GOL.c
+++ GOL.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+// #define DEBUG
 /*
  * RA: 20866585	Victor de Mattos Souza
  * RA: 20751756 Matheus Felipe Fischer
  */
+
+struct args{
+	int i;
+	int j;
+	int size;
+	char* buffer;
+	char* nextGenBuffer;
+};
+
 int countNeighbors(char* buffer, int i, int j, int size);
-void aliveOrDeath(char i, int j, char* buffer, int countNeighbors, char* nextGenBuffer, int size);
+void *aliveOrDeath(void *arg_p);
 void nextGen(char* buffer, char* nextGenBuffer, int* generation, int size);
 void draw(FILE* output, char* buffer, int size);
 void update(char* buffer, char* nextGenBuffer, int size);
 char get(char* buffer, int size, int i, int j);
 void set(char* buffer, int size, int i, int j, char value);
 int checksum(char* buffer, int size);
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char **argv) {
 
@@ -35,9 +48,9 @@ int main(int argc, char **argv) {
 		for(j = 0; j < size; j++) {
 			fscanf(input, "%c", &inp);
 			set(buffer, size, i, j, inp);
-			// printf("%c", get(buffer, size, i, j)  );
+			printf("%c", get(buffer, size, i, j)  );
 		}
-		// printf("\n");
+		printf("\n");
 		fscanf(input, "\n");
 	}
 	
@@ -67,9 +80,26 @@ void nextGen(char* buffer, char* nextGenBuffer, int* generation, int size){
 void update(char* buffer, char* nextGenBuffer, int size) {
 	int i, j;
 	for(i = 0; i < size; i++) {
-		for(j = 0; j < size; j++) {
-			int count = countNeighbors(buffer, i, j, size);
-			aliveOrDeath(i, j, buffer, count, nextGenBuffer, size);
+		for(j = 0; j < size; j = j + 3) {
+			pthread_t thread_1, thread_2, thread_3;
+			struct args args_1 = {i, j, size, buffer, nextGenBuffer};
+			struct args args_2 = {i, j + 1, size, buffer, nextGenBuffer};
+			struct args args_3 = {i, j + 2, size, buffer, nextGenBuffer};
+
+			struct args *p_args_1 = &args_1;
+			struct args *p_args_2 = &args_2;
+			struct args *p_args_3 = &args_3;
+
+			int one = pthread_create(&thread_1, NULL, aliveOrDeath, (void*) p_args_1);
+			int two = pthread_create(&thread_2, NULL, aliveOrDeath, (void*) p_args_2);
+			int tree = pthread_create(&thread_3, NULL, aliveOrDeath, (void*) p_args_3);
+
+			pthread_join(one, NULL);
+			pthread_join(two, NULL);
+			pthread_join(tree, NULL); 
+			#ifdef DEBUG
+				draw(stdout, buffer, size);
+			#endif
 		}
 	}
 }
@@ -84,20 +114,28 @@ int countNeighbors(char* buffer, int i, int j, int size){
     int SouthEast = ((i == size-1 || j == size-1)? 0 : (get(buffer, size, i+1, j+1) == '1' ? 1 : 0));
     return North + South + East + West + NorthEast + NorthWest + SouthEast + SouthWest;
 }
-void aliveOrDeath(char i, int j, char* buffer, int countNeighbors, char*nextGenBuffer, int size){
-	if((get(buffer, size, i, j) == '1') && (countNeighbors > 3 || countNeighbors < 2)){
-		set(nextGenBuffer, size, i, j, '0');
-		return;
+void *aliveOrDeath(void* arg_p){
+	pthread_mutex_lock(&mutex);
+	struct args *arg = arg_p;
+	int count = countNeighbors(arg->buffer, arg->i, arg->j, arg->size);
+	if((get(arg->buffer, arg->size, arg->i, arg->j) == '1') && (count > 3 || count < 2)){
+		set(arg->nextGenBuffer, arg->size, arg->i, arg->j, '0');
+		pthread_mutex_unlock(&mutex);
+		return NULL;
 	}
-	if (get(buffer, size, i, j) == '0' && countNeighbors == 3){
-		set(nextGenBuffer, size, i, j, '1');
-		return;
+	if (get(arg->buffer, arg->size, arg->i, arg->j) == '0' && count == 3){
+		set(arg->nextGenBuffer, arg->size, arg->i, arg->j, '1');
+		pthread_mutex_unlock(&mutex);
+		return NULL;
 	}
-	if(get(buffer, size, i, j) == '1' && (countNeighbors <= 3 || countNeighbors >= 2)){
-		set(nextGenBuffer, size, i, j, '1');
-		return;
+	if(get(arg->buffer, arg->size, arg->i, arg->j) == '1' && (count <= 3 || count >= 2)){
+		set(arg->nextGenBuffer, arg->size, arg->i, arg->j, '1');
+		pthread_mutex_unlock(&mutex);
+		return NULL;
 	}
-	set(nextGenBuffer, size, i, j, get(buffer, size, i, j));
+	set(arg->nextGenBuffer, arg->size, arg->i, arg->j, get(arg->buffer, arg->size, arg->i, arg->j));
+	pthread_mutex_unlock(&mutex);
+	return NULL;
 }
 void draw(FILE* output, char * buffer, int size) {
 	int i, j;
